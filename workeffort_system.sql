5f4a359e3318aa3a32d8e5f085bd476975ac416f
unknown <astridfrillya@gmail.com>
2014-10-02 23:22:03 +0700

    baru

--- workeffort_system.sql
+++ workeffort_system.sql
@@ -23,3 +23,28 @@ constraint fk_work_req FOREIGN KEY (req_type_id) REFERENCES req_type);
  deliverable varchar2(50),
  constraint pk_work_req_type PRIMARY KEY (wrt_id),
  constraint fk_work_req_type FOREIGN KEY (work_req_id) REFERENCES work_req);
+ 
+ create table req_role_type(
+ rrt_id varchar2(10),
+ des varchar2(30),
+ constraint pk_rrt PRIMARY KEY (rrt_id));
+ 
+create table party_work_req_role(
+pwrr_id varchar2(10),
+work_req_id varchar2(10),
+rrt_id varchar2(10),
+from_date varchar2(20),
+thru_date varchar2(20),
+constraint pk_pwrr PRIMARY KEY (pwrr_id),
+constraint fk_pwrr FOREIGN KEY (work_req_id) REFERENCES work_req,
+constraint fk_pwrr2 FOREIGN KEY (rrt_id) REFERENCES req_role_type);
+
+create table we_from_work_req(
+we_id varchar2(10),
+name varchar2(20),
+des varchar2(50),
+start_date varchar2(20),
+work_req_id varchar2(10),
+constraint pk_wfwr PRIMARY KEY (we_id),
+constraint fk_wfwr FOREIGN KEY (work_req_id) REFERENCES work_req);
+
